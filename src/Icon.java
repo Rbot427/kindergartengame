86e220b9a35d1e4d25a690d127f48cbea30bbb44
Rbot427 <rbot427@gmail.com>
2016-03-08 10:45:42 -0500

    Added methods

--- src/Icon.java
+++ src/Icon.java
@@ -3,16 +3,32 @@ import java.awt.*;
 public class Icon {
   private Image img;
   private int posX, posY;
-  private int size;
+  private int height, width;
   
   public Icon(Image image, int x, int y) {
     img = image;
     posX = x; posY =  y;
+    height = img.getHeight();
+    width = img.getWidth();
   }
   
   public Icon(Image image) {
     Icon(image, 0, 0);
   }
   
+  public void setPos(int x, int y) {
+    
+  }
   
+  private void updateSize() {
+    height = img.getHeight();
+    width = img.getWidth();
+  }
+  
+  public void setImage(Image img) {
+    img = image;
+    updateSize();
+  }
+  
+  public 
 }
