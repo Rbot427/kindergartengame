f1380dff0dd5083b80894a4dacfe81dc68fe1174
Ryan Cheese <rbot427@gmail.com>
2016-04-01 09:52:48 -0400

    Began development of menu

---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+public class Menu extends JComponentWithEvents {
+  public String trickGame = "TrickGame.java";
+  public String shapeGame = "Shapes.java";
+  public String addingGame = "AddingWithLion.java";
+  
+  public static void main(String[] args) {
+    launch(800, 600);
+  }
+}
